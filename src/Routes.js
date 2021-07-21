447e0f0c97ae5bc9bcd688b55552526c89555636
siddhesh <siddheshs871@gmail.com>
2021-07-21 12:58:28 +0530

    ss of inshort-clone uploaded

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import NewsApp from './core/NewsApp/NewsApp'
-
+import Weather from './core/WeatherApp/Weather'
 
 
 
@@ -10,7 +10,7 @@ const Routes = () => {
         <BrowserRouter>
     <Switch>
         <Route path='/' exact  component={NewsApp} ></Route>
-        {/* <Route path='/weather' exact component={Weather} ></Route> */}
+        <Route path='/weather' exact component={Weather} ></Route>
         
 
     </Switch>
