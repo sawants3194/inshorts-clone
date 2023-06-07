3d6dedb27b72d1e685029caa5228a6e6216786c6
siddhesh <siddheshs871@gmail.com>
2023-06-07 19:59:21 +0530

    weather is in progress

--- src/core/NewsApp/NewsApp.js
+++ src/core/NewsApp/NewsApp.js
@@ -8,49 +8,54 @@ import Menu from '../../Menu';
 
 
 const NewsApp = () => {
-    const [category, setCategory] = useState("general");
-    const [newsArray, setnewsArray] = useState([]);
-    const [newsResults, setnewsResults] = useState();
-    const [loadMore, setLoadMore] = useState(20);
-  
-    // console.log(process.env);
-    // console.log(NEWS_API);
-  
-    useEffect(() => {
-      const newsApi = async () => {
-        await axios
-          .get(
-            `https://newsapi.org/v2/top-headlines?country=in&apiKey=${NEWS_API}&category=${category}&pageSize=${loadMore}`
-          )
-          .then(function (response) {
-            // handle success
-            console.log("news", response);
-            setnewsArray(response.data.articles);
-            setnewsResults(response.data.totalResults);
+  const [category, setCategory] = useState("general");
+  const [newsArray, setnewsArray] = useState([]);
+  const [newsResults, setnewsResults] = useState();
+  const [loadMore, setLoadMore] = useState(40);
+
+  // console.log(process.env);
+  // console.log(NEWS_API);
+  var config = {
+    headers: { 'Access-Control-Allow-Origin': '*' }
+  };
+  useEffect(() => {
+    const newsApi = async () => {
+      await axios
+        .get(
+          `https://newsapi.org/v2/top-headlines?country=in&apiKey=${NEWS_API}&category=${category}&pageSize=${loadMore}`
+          , config)
+        .then(function (response) {
+          // handle success
+          console.log(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${NEWS_API}&category=${category}&pageSize=${loadMore}`)
+          setnewsArray(response.data.articles);
+          newsArray.map(n => {
+            console.log(n)
           })
-          .catch(function (error) {
-            // handle error
-            console.log(error);
-          });
-      };
-  
-      newsApi();
-    }, [newsResults, category, loadMore]);
-    return (
-      <div className="App">
-      <Menu/>
-        <NavInshorts setCategory={setCategory} />
-        {newsResults && (
-          <NewsContent
-            newsArray={newsArray}
-            newsResults={newsResults}
-            loadMore={loadMore}
-            setLoadMore={setLoadMore}
-          />
-        )}
-        <Footer />
-      </div>
-    );
+          setnewsResults(response.data.totalResults);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log("Error", error.message);
+        });
+    };
+
+    newsApi();
+  }, [newsResults, category, loadMore]);
+  return (
+    <div className="App">
+      <Menu />
+      <NavInshorts setCategory={setCategory} />
+      {newsResults && (
+        <NewsContent
+          newsArray={newsArray}
+          newsResults={newsResults}
+          loadMore={loadMore}
+          setLoadMore={setLoadMore}
+        />
+      )}
+      <Footer />
+    </div>
+  );
 }
 
 export default NewsApp
