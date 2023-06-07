31d8fe99ae8b85da795f0c0ee84f33ab565f9373
siddhesh <siddheshs871@gmail.com>
2023-06-07 20:35:28 +0530

    weather is in progress

--- src/core/NewsApp/NavInshorts.js
+++ src/core/NewsApp/NavInshorts.js
@@ -1,13 +1,12 @@
 import React from "react";
 import './Style.css'
 
-import HamburgerDrawer from './HamburgerDrawer'
 const NavInshorts = ({setCategory}) => {
   return (
     <div className="nav">
-      <div className="icon">
+      {/* <div className="icon">
         <HamburgerDrawer setCategory={setCategory} />
-      </div>
+      </div> */}
      
         <img
           src="https://assets.inshorts.com/website_assets/images/logo_inshorts.png"
