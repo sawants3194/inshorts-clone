2012473d2176d82b62770d644d635561428ade94
sawants3194 <sawants3194@gmail.com>
2025-05-29 23:29:20 +0530

    app works fine

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';  // Import from 'react-dom/client' in React 18
+import ReactDOM from 'react-dom';  // Import from 'react-dom/client' in React 18
 import Routes from './Routes';
 import 'semantic-ui-css/semantic.min.css';
 import AppRoutes from "./Routes";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppRoutes />);
+ReactDOM.render(<AppRoutes />, document.getElementById('root'));
